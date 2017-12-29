443d3f321d4d22d92526ad41b437023510341a55
Jack Graham <jackmgraham@hotmail.co.uk>
2017-12-29 14:31:11 +0000

    init commit

---
+++ app/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    verify = require('./verify'),
+    monzo = require('./monzo');
+
+let app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json({
+    verify: function getRawBody(req, res, buf) {
+        req.rawBody = buf.toString();
+    }
+}));
+
+
+app.get('/', function(req, res) {
+    res.json({ message: 'The monzo skill is up and running.', since: (new Date()).toString() });
+});
+app.post('/balance', verify, monzo);
+
+
+app.listen(app.get('port'), function() {
+    console.log('Monzo skill is up and running on port %d', app.get('port'));
+});
---
+++ app/monzo.js
@@ -0,0 +1,143 @@
+"use strict";
+
+let request = require("request");
+
+const VERSION = "1.0";
+const access_token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaSI6Im9hdXRoY2xpZW50XzAwMDA5NFB2SU5ER3pUM2s2dHo4anAiLCJleHAiOjE1MTQ0ODAxMjAsImlhdCI6MTUxNDQ1ODUyMCwianRpIjoidG9rXzAwMDA5UzEyYzZ6NXpUMEI1bndxUzkiLCJ1aSI6InVzZXJfMDAwMDk2RmFETEdNcmdjQjdtVFhJZiIsInYiOiIyIn0.MKSDASkUBNRAJegLb33TNN-yve9vEnN1ynsr5V9dE4g";
+const BASE_URL = "https://api.monzo.com/";
+
+module.exports = function(req, res) {
+  if (req.body.request.type === "LaunchRequest") {
+    res.json(
+      buildResponse(
+        { dateRequested: true },
+        "<speak>Hello Jack, this is your monzo skill. Ask for your balance.</speak>",
+        {},
+        false
+      )
+    );
+  } else if (req.body.request.type === "SessionEndedRequest") {
+    if (req.body.request.reason === "ERROR") {
+      console.error("Alexa ended the session due to an error");
+    }
+  } else if (
+    req.body.request.type === "IntentRequest" &&
+    req.body.request.intent.name === "Balance"
+  ) {
+    getBalance()
+      .then(function(balance) {
+        res.json(
+          buildResponse(
+            {},
+            "<speak>" + balance.text + "</speak>",
+            balance.card,
+            true
+          )
+        );
+      })
+      .catch(function(err) {
+        res.json(buildResponse({}, "<speak>" + err + "</speak>", {}, true));
+      });
+  } else {
+    res
+      .status(504)
+      .json({
+        message: "Sorry that Intent has not been added to our skill set"
+      });
+  }
+};
+
+function buildResponse(session, speech, card, end) {
+  return {
+    version: VERSION,
+    sessionAttributes: session,
+    response: {
+      outputSpeech: {
+        type: "SSML",
+        ssml: speech
+      },
+      card: card,
+      shouldEndSession: !!end
+    }
+  };
+}
+
+function getBalance() {
+  return new Promise(function(resolve, reject) {
+    request(
+      {
+        url: BASE_URL + "balance?account_id=acc_000097O438ahiqClRKOtU1",
+        headers: {
+          Authorization: `Bearer ${access_token}`
+        },
+        json: true
+      },
+      function(err, res, body) {
+        let data, text, card;
+
+        if (err || res.statusCode >= 400) {
+          console.error(res.statusCode, err);
+          return reject("Unable to get balance!");
+        }
+
+        if (!data) {
+          return reject("Unable to get balance!");
+        }
+
+        text = getBalanceText(data);
+        card = {
+          type: "Standard",
+          title: "Monzo card balance",
+          text: text
+        };
+
+        resolve({ text, card });
+      }
+    );
+  });
+}
+
+function getBalanceText(data) {
+  let conditions;
+
+  if (data.balance) {
+    const amountParts = (amount / 100)
+      .toFixed(2)
+      .toString()
+      .split(".");
+
+    const majorUnits = +amountParts[0];
+    const minorUnits = +amountParts[1];
+
+    const responseParts = [];
+    if (majorUnits !== 0 || minorUnits === 0)
+      responseParts.push(
+        `${majorUnits} ${currencyDefinition["GBP"].majorCurrencyUnit(
+          majorUnits
+        )}`
+      );
+
+    if (minorUnits !== 0 || majorUnits === 0)
+      responseParts.push(
+        `${minorUnits} ${currencyDefinition["GBP"].minorCurrencyUnit(
+          minorUnits
+        )}`
+      );
+
+    conditions = responseParts.join(" and ");
+  }
+
+  return conditions;
+}
+
+const currencyDefinition = {
+  GBP: {
+    majorCurrencyUnit(amount) {
+      return amount === 1 ? "pound" : "pounds";
+    },
+    minorCurrencyUnit(amount) {
+      return "pence";
+    }
+  }
+};
